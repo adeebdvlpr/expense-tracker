a7561ec30ea93f1b9722f9c63efd94d3e6bc07b6
Adeebd28 <152583785+adeebdvlpr@users.noreply.github.com>
2026-04-11 20:43:15 -0700

    vvv

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -179,34 +179,3 @@ const Register = ({ onSubmit, isLoading, onCancel }) => {
 };
 
 export default Register;
-
-/*
-Use something lik ethis for PASSWORD WHEN I GET BACK + Finish front end (client) changes for new form --- thinking 
-email + username?+ password required (visibilioty eye)....make the rest optional. Then have an account 
-area where they can add that to account post initial registration 
-
-_______
-
-<FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label={
-                    showPassword ? 'hide the password' : 'display the password'
-                  }
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  onMouseUp={handleMouseUpPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-*/
